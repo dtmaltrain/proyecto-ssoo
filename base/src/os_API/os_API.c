f3f7a6d036e78da6fd641c3fc48ea187ce7b46cd
tfflores <tfflores@uc.cl>
2021-05-22 22:39:06 -0400

    Archivos iniciales y MakeFile (vacíos)

---
+++ base/src/os_API/os_API.c
@@ -0,0 +1,7 @@
+// Import used global libraries
+#include <stdio.h>  // FILE, fopen, fclose, etc.
+#include <string.h> // strtok, strcpy, etc.
+#include <stdlib.h> // malloc, calloc, free, etc.
+
+// Import the header file of this module
+#include "os_API.h"
---
+++ base/src/osfs/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  printf("Hello P1!\n");
+  printf("Goodbye P1!\n");
+}
